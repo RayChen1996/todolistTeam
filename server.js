4b6c872f9462c0423d081e0c53ff8090151d8a3c
Tr-blank <blank0129@gmail.com>
2024-04-15 00:53:41 +0800

    feat: aujset server.js

--- server.js
+++ server.js
@@ -1,33 +1,43 @@
 const http = require('http');
-const { v4: uuidv4 } = require('uuid');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const headers = require('./headersSetting');
+const successHandle = require('./successHandle');
+const postTodo = require('./postTodo');
+const getTodo = require('./getTodo');
+const patchTodo = require('./patchTodo');
+const deleteTodo = require('./deleteTodo');
 
-const requestListener = (req, res)=>{
-    let body = "";
-    
-    req.on('data', chunk=>{
-        body+=chunk;
-    })
-    
-    if(req.url=="/todos" && req.method == "GET"){
-        // getTodo.js
-    }else if(req.url=="/todos" && req.method == "POST"){
-        // postTodo.js
-    }else if(req.url=="/todos" && req.method == "DELETE"){
-        // deleteTodo.js
-    }else if(req.url.startsWith("/todos/") && req.method=="DELETE"){
-        // deleteTodo.js
+dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB) // 連線資料庫
+  .then(() => {
+    console.log('資料庫連線成功');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const requestListener = (req, res)=>{    
+    if(req.url=='/todos' && req.method == 'GET'){
+        getTodo(res);
+    }else if(req.url=='/todos' && req.method == 'POST'){
+        postTodo(res, req);
+    }else if(req.url=='/todos' && req.method == 'DELETE'){
+        deleteTodo(res);
+    }else if(req.url.startsWith('/todos/') && req.method=='DELETE'){
         deleteTodo(res, req);
-    }else if(req.url.startsWith("/todos/") && req.method=="PATCH"){
-        // patchTodo.js
-    }else if(req.method == "OPTIONS"){
-        res.writeHead(200,headers);
-        res.end();
+    }else if(req.url.startsWith('/todos/') && req.method=='PATCH'){
+        patchTodo(res, req);
+    }else if(req.method == 'OPTIONS'){
+        successHandle(res);
     }else{
         res.writeHead(404,headers);
         res.write(JSON.stringify({
-            "status": "false",
-            "message": "無此網站路由"
+            'status': 404,
+            'message': '無此網站路由'
         }));
         res.end();
     }
