fb24c6fae02bda97345be47b54893eec3452b1e8
RayChen <149983470+raychen19960102@users.noreply.github.com>
2024-04-14 19:40:02 +0800

    update postTodo 、 getTodos

---
+++ postTodo.js
@@ -0,0 +1,27 @@
+const Todos = require("./model/todo");
+const successHandle = require("./successHandle");
+const errorHandle = require("./errorHandle");
+const { v4: uuidv4 } = require("uuid");
+
+/** - 新增 */
+function postTodo(req, res) {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  req.on("end", async () => {
+    try {
+      const _title = JSON.parse(body)?.title;
+      const _newTodo = await Todos.create({
+        _title,
+      });
+
+      successHandle(res, _newTodo);
+    } catch (error) {
+      errorHandle(res);
+    }
+  });
+}
+
+module.exports = postTodo;
