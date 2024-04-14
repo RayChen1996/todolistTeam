9cfdb870a1a3e2ab053c11b6ce465a69468012d3
Tr-blank <blank0129@gmail.com>
2024-04-14 15:52:57 +0800

    add headersSetting.js and successHandle.js

---
+++ successHandle.js
@@ -0,0 +1,15 @@
+const headers = require('./headersSetting');
+
+function successHandle(res, data) {
+    res.writeHead(200, headers);
+    res.write(
+      JSON.stringify({
+        status: "success",
+        data,
+      })
+    );
+    res.end();
+  }
+  
+  module.exports = successHandle;
+  
