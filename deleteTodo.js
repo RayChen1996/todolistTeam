fb128e88347a7ce0707a9567c1e510f8b4c5dd05
Tr-blank <blank0129@gmail.com>
2024-04-15 00:52:19 +0800

    fix: deletaTodo.js

--- deleteTodo.js
+++ deleteTodo.js
@@ -1,14 +1,14 @@
-const Todos = require("./models/todo");
-const successHandle = require("./successHandle");
-const errorHandle = require("./errorHandle");
+const Todos = require('./models/todo');
+const successHandle = require('./successHandle');
+const errorHandle = require('./errorHandle');
 
 const deleteTodo = async (res, req = null) => {
   if (!req) {
-    const todos = Todos.deleteMany({});
+    const todos = await Todos.deleteMany({});
     successHandle(res, todos);
   } else {
     try {
-      const id = req.url?.split("/")?.pop();
+      const id = req.url?.split('/')?.pop();
       const result = await Todos.findByIdAndDelete(id);
 
       successHandle(res, result);
